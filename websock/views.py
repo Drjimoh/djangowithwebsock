89e5a077463e30db70bad9a7c584f1ab85b60b9f
walili <waliu.jimoh@yahoo.com>
2019-08-19 09:25:34 +0100

    Update views.py

--- websock/views.py
+++ websock/views.py
@@ -1,11 +1,5 @@
 from django.shortcuts import render
-from django.utils.safestring import mark_safe
-import json
 
-
-'''def index(request):
-    return render(request, 'websock/index.html',
-                  {})'''
 
 def index(request):
     return render(request, 'websock/index.html', {})
